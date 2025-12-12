354d09ec43203fa9327bb730bab667e6b10c7e85
Sammy Kariuki <sammykariuki099@gmail.com>
2025-12-12 11:28:29 +0300

    add Banking display choices

--- src/Bank.java
+++ src/Bank.java
@@ -5,19 +5,32 @@ public class Bank {
         //declare variables
         double balance = 0;
         int choice;
+        boolean isRunning = true;
         Scanner scanner = new Scanner(System.in);
 
         //welcome message
         System.out.println("***********************************");
         System.out.println("Welcome to the Java Banking Program");
         System.out.println("***********************************");
-        System.out.println("1. Show Balance");
-        System.out.println("2. Show Balance");
-        System.out.println("3. Show Balance");
-        System.out.println("4. Show Balance");
         //display options
-        System.out.print("Select option (1-4): ");
-        choice = scanner.nextInt();
+        do{
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+
+            System.out.print("Select option (1-4): ");
+            choice = scanner.nextInt();
+
+            if(choice < 1 || choice > 4) {
+                System.out.println("Invalid Choice");
+            }
+            if (choice == 4) {
+                System.out.println("Thank You. GOODBYE");
+                isRunning = false;
+            }
+        } while(isRunning);
+
         //  showBalance
         //  deposit
         //  withdraw
