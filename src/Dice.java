b983ab412a6d5d3af2b2385e04f9d6bdd9141b36
Sammy Kariuki <sammykariuki099@gmail.com>
2025-12-13 21:22:37 +0300

    initialized Dice Roller Programme

---
+++ src/Dice.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Dice {
+    public static void rollDice() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int dice = 0;
+
+        System.out.println("*****************************************");
+        System.out.println("Welcome to the Java Dice Roller Programme");
+        System.out.println("*****************************************");
+
+        //user input no of dice
+        while (dice < 1 || dice > 4){
+            System.out.print("How many dice do you want to roll? (1-4)");
+            dice = scanner.nextInt();
+            if(dice < 1 || dice > 4) {
+                System.out.println("Invalid input");
+            }
+        }
+        //output
+    }
+}
