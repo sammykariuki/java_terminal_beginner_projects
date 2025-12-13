2ce27c19672612880714b49fba7aa666c891909e
Sammy Kariuki <sammykariuki099@gmail.com>
2025-12-13 21:36:55 +0300

    add dice display Dice Roller Programme

--- src/Dice.java
+++ src/Dice.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 public class Dice {
+
     public static void rollDice() {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
@@ -12,12 +13,73 @@ public class Dice {
 
         //user input no of dice
         while (dice < 1 || dice > 4){
-            System.out.print("How many dice do you want to roll? (1-4)");
+            System.out.print("How many dice do you want to roll? (1-4): ");
             dice = scanner.nextInt();
             if(dice < 1 || dice > 4) {
+                System.out.println("-------------");
                 System.out.println("Invalid input");
+                System.out.println("-------------");
             }
         }
         //output
+    }
+    static void showDice() {
+        switch() {
+            case 1 -> System.out.println("""
+                      _ _ _ _ _
+                    |           |
+                    |           |
+                    |     ●     |
+                    |           |
+                    |           |
+                      _ _ _ _ _
+                    """);
+            case 2 -> System.out.println("""
+                      _ _ _ _ _
+                    |           |
+                    |  ●        |
+                    |           |
+                    |        ●  |
+                    |           |
+                      _ _ _ _ _
+                    """);
+            case 3 -> System.out.println("""
+                      _ _ _ _ _
+                    |           |
+                    |  ●        |
+                    |     ●     |
+                    |        ●  |
+                    |           |
+                      _ _ _ _ _
+                    """);
+            case 4 -> System.out.println("""
+                      _ _ _ _ _
+                    |           |
+                    |  ●     ●  |
+                    |           |
+                    |  ●     ●  |
+                    |           |
+                      _ _ _ _ _
+                    """);
+            case 5 -> System.out.println("""
+                      _ _ _ _ _
+                    |           |
+                    |  ●     ●  |
+                    |     ●     |
+                    |  ●     ●  |
+                    |           |
+                      _ _ _ _ _
+                    """);
+            case 6 -> System.out.println("""
+                      _ _ _ _ _
+                    |           |
+                    |  ●     ●  |
+                    |  ●     ●  |
+                    |  ●     ●  |
+                    |           |
+                      _ _ _ _ _
+                    """);
+            default -> System.out.println("Invalid roll");
+        }
     }
 }
